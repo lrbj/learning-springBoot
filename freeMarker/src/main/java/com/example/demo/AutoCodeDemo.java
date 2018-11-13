28d83d998824b73af6176f77171a566589b05559
H299633 <shuzhen.ye@honeywell.com>
2018-11-13 16:38:25 +0800

    first commit

---
+++ freeMarker/src/main/java/com/example/demo/AutoCodeDemo.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class AutoCodeDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hell world");
+    }
+
+}
---
+++ freeMarker/src/test/java/com/example/demo/FreeMarkerTest.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: Kayla, Ye
+ * @Description:
+ * @Date:Created in 4:01 PM 11/13/2018
+ */
+public class FreeMarkerTest {
+    //模板路径
+    private static final String TEMPLATE_PATH = "D:\\learn\\javaweb\\freeMarker\\demo\\src\\main\\resources\\templates";
+    //要生成的类路径
+    private static final String CLASS_PATH = "D:\\learn\\javaweb\\freeMarker\\demo\\src\\main\\java\\com\\example\\demo";
+
+    @Test
+    public  void testFreemarker() throws Exception {
+        //1、创建配置实例
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_20);
+
+
+        //2、获取模板路径
+        configuration.setDirectoryForTemplateLoading(new File(TEMPLATE_PATH));
+        //3、创建数据模型
+        Map<String,String> map = new HashMap<>();
+        map.put("classPath","com.example.demo");
+        map.put("className","AutoCodeDemo" );
+        map.put("message","hell world");
+
+        //4、加载模板文件
+        Template template = configuration.getTemplate("hello.ftl");
+
+        //5、生成数据
+        File file = new File(CLASS_PATH+"\\"+"AutoCodeDemo.java");
+        Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+
+        //6、输出文件
+        template.process(map, out);
+
+    }
+}
